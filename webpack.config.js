de770cde47b317f484458a8b8fe1a2787b083a77
Eliya Vahl <eliyavahl@gmail.com>
2022-02-06 01:12:49 +0200

    fix: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Development",
+      favicon: "./favicon.ico",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
