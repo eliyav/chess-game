ef4213b5d34952468f81ec24657176836071f166
Eliya Vahl <eliyavahl@gmail.com>
2022-07-11 02:25:49 +0300

    build: use-authentication hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,79 @@
-import React, { useEffect, useState } from "react";
-import { Content } from "./content";
-import { useAuth0 } from "@auth0/auth0-react";
-import { getUserInfo } from "./helper/request-helpers";
+import * as BABYLON from "babylonjs";
+import React, { useEffect, useRef, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import LoadingScreen from "./component/loading-screen";
+import { useAuthentication } from "./hooks/use-authentication";
+import { Home } from "./routes/home";
+import { JoinLobby } from "./routes/join-match";
+import { Matches } from "./routes/matches";
+import { OfflineGameView } from "./routes/offline-game-view";
+import { OfflineLobby } from "./routes/offline-lobby";
+import { OnlineGameView } from "./routes/online-game-view";
+import { OnlineLobby } from "./routes/online-lobby";
+import { displayScreen } from "./view/display-screen";
 
 const App: React.FC = () => {
+  const [isInitDone, setIsInitDone] = useState(false);
+  const [loadingScreen, setloadingScreen] = useState(true);
   const [currentUser, setCurrentUser] = useState<UserData>();
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [socketConnection, setSocketConnection] = useState<any>();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const location = useLocation();
+  const [user, isAuthenticated] = useAuthentication(setCurrentUser);
 
   useEffect(() => {
-    if (isAuthenticated)
+    if (isInitDone) {
+      let engine = new BABYLON.Engine(canvasRef.current, true);
       (async () => {
-        try {
-          const token = await getAccessTokenSilently();
-          const response = await fetch(`${window.location.host}/login`, {
-            method: "POST",
-            headers: {
-              authorization: `Bearer ${token}`,
-            },
-            body: getUserInfo(user!),
-          });
-          const userData: UserData = await response.json();
-          setCurrentUser(userData);
-        } catch (err) {
-          console.error(err);
-        }
+        const displayScene = await displayScreen(engine);
+        engine.runRenderLoop(() => {
+          displayScene.render();
+          engine.resize();
+        });
+        setTimeout(() => setloadingScreen(false), 1000);
       })();
-  }, [isAuthenticated]);
+      return () => engine.stopRenderLoop();
+    }
+    setIsInitDone(true);
+  }, [isInitDone]);
 
   return (
     <div id="app">
-      <Content userData={currentUser} />
+      <canvas ref={canvasRef} className="displayCanvas screen"></canvas>
+      <Routes>
+        <Route
+          path="/"
+          element={loadingScreen ? <LoadingScreen text="..." /> : <Home />}
+        />
+        <Route path="/match" element={<Matches />}>
+          <Route path="/match/offline-lobby" element={<OfflineLobby />} />
+          <Route
+            path="/match/online-lobby"
+            element={
+              <OnlineLobby
+                setSocket={setSocketConnection}
+                userName={currentUser!.name}
+              />
+            }
+          />
+          <Route
+            path="/match/join-lobby"
+            element={
+              <JoinLobby
+                setSocket={setSocketConnection}
+                userName={currentUser!.name}
+              />
+            }
+          />
+        </Route>
+        <Route path="/offline-game" element={<OfflineGameView />} />
+        <Route
+          path="/online-game"
+          element={
+            <OnlineGameView location={location} socket={socketConnection} />
+          }
+        />
+      </Routes>
     </div>
   );
 };
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,39 +1,5 @@
-import * as BABYLON from "babylonjs";
-import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect, useRef, useState } from "react";
-import { displayScreen } from "../view/display-screen";
-import LoadingScreen from "../component/loading-screen";
+import React from "react";
 
 export const Home: React.FC = () => {
-  const [isInitDone, setIsInitDone] = useState(false);
-  const [loadingScreen, setloadingScreen] = useState(true);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const { loginWithPopup, isAuthenticated, user } = useAuth0();
-
-  useEffect(() => {
-    if (isInitDone) {
-      let engine = new BABYLON.Engine(canvasRef.current, true);
-      (async () => {
-        const displayScene = await displayScreen(engine);
-        engine.runRenderLoop(() => {
-          displayScene.render();
-          engine.resize();
-        });
-        setTimeout(() => setloadingScreen(false), 1000);
-      })();
-      return () => engine.stopRenderLoop();
-    }
-    setIsInitDone(true);
-  }, [isInitDone]);
-
-  return (
-    <>
-      <canvas ref={canvasRef} className="displayCanvas screen"></canvas>
-      {loadingScreen ? (
-        <LoadingScreen text="..." />
-      ) : (
-        <div className="home screen"></div>
-      )}
-    </>
-  );
+  return <div className="home screen"></div>;
 };
