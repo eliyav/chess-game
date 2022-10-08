24dd2053779081e29a2a966bb2be50beb9c5306a
Eliya Vahl <eliyavahl@gmail.com>
2022-10-09 00:57:49 +0300

    build: more home page updates

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const menuItems = ["Play", "Options", "Login"];
+const menuItems = ["Login", "Play"];
+
+//add to the play button
+// background: #007e008f;
 
 export const Home: React.FC = () => {
   return (
     <div className="home screen">
-      <h1 className="home-title">CHESS</h1>
+      <h1 className="home-title">3D CHESS</h1>
       <div className="menu">
         {menuItems.map((item) => (
           <div className="menu-item">
