82c4eba8f4b0b789674234bb2532e0d447930c2e
Eliya Vahl <eliyavahl@gmail.com>
2022-11-05 15:25:06 +0200

    build: lobby initial commit

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { SvgGithub } from "../svg-components/svg-github";
 
-const menuItems = [{ text: "Play", path: "/game" }];
+const menuItems = [{ text: "Play", path: "/lobby" }];
 
 export const Home: React.FC = () => {
   return (
--- src/routes/lobby.tsx
+++ src/routes/lobby.tsx
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-interface Lobby {}
+export const Lobby: React.FC<{}> = () => {
+  const [mode, setMode] = useState<LobbyModes>("offline");
+  const [opponent, setOpponent] = useState("human");
+  const [time, setTime] = useState("0");
 
-export const Lobby: React.FC<Lobby> = () => {
   return (
     <div className="lobby screen">
-      <h1 className="sub-title">Lobby</h1>;{/* Game Settings */}
-      {/* Start Game Button at bottom */}
-      <button onClick={() => {}}>Start</button>
+      <h1 className="sub-title">Lobby</h1>;
+      <div className="lobby-modes">
+        <h2 className="label">Mode</h2>
+        <div className="selections">
+          <div
+            onClick={() => setMode("offline")}
+            className={`selection ${mode === "offline" && "highlight"}`}
+          >
+            Offline
+          </div>
+          <div
+            className={`selection ${mode === "online" && "highlight"}`}
+            onClick={() => setMode("online")}
+          >
+            Online
+          </div>
+        </div>
+      </div>
+      {mode === "online" && (
+        <div className="lobby-code">
+          <h2 className="label">Invite Code</h2>
+          <p>ABCDEF</p>
+        </div>
+      )}
+      <div className="opponent">
+        <h2 className="label">Opponent</h2>
+        {mode === "offline" && (
+          <div className="selections">
+            <div className={`selection ${opponent === "human" && "highlight"}`}>
+              Human
+            </div>
+            {/* <div className="selection">{"Bot (Coming Soon)"}</div> */}
+          </div>
+        )}
+      </div>
+      <div className="clock">
+        <h2 className="label">Clock</h2>
+        <div className="clock-selections">
+          <input
+            className="slider"
+            name="time"
+            type="range"
+            min="0"
+            max="60"
+            step="5"
+            defaultValue="0"
+            onChange={(e) => setTime(e.currentTarget.value)}
+          ></input>
+          <p className="time-display">{time === "0" ? "No Limit" : time}</p>
+        </div>
+      </div>
+      <div className="menu">
+        <Link to={"/game"}>
+          <button className="menu-btn">Start</button>
+        </Link>
+      </div>
     </div>
   );
 };
+
+type LobbyModes = "offline" | "online";
