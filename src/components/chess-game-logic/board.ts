9a91ec82424d925e6f55c85e5d2137800b53ce34
Eliya Vahl <eliyavahl@gmail.com>
2023-08-31 03:24:38 +0300

    refactor(board): creation and set logic

--- src/components/chess-game-logic/board.ts
+++ src/components/chess-game-logic/board.ts
@@ -1,51 +1,89 @@
-import chessData from "./chess-data-import";
 import GamePiece from "./game-piece";
 
 class Board {
-  movementArray: number[];
-  pieceInitialPoints: typeof chessData.pieceInitialPoints;
   boardSize: number;
   columnNames: string[][];
-  grid: Square[][];
+  board: Square[][];
 
   constructor() {
-    this.movementArray = chessData.movement;
-    this.pieceInitialPoints = chessData.pieceInitialPoints;
-    this.boardSize = chessData.boardSize;
-    this.columnNames = chessData.columnNames;
-    this.grid = this.createGrid();
+    this.boardSize = 8;
+    this.columnNames = [["a"], ["b"], ["c"], ["d"], ["e"], ["f"], ["g"], ["h"]];
+    this.board = [];
+    this.setBoard();
+  }
+
+  public setBoard() {
+    this.board = this.createBoard();
     this.setPieces();
   }
 
-  setPieces() {
-    this.pieceInitialPoints.forEach((array) =>
-      array.forEach((ele) => {
-        const { name, color, points } = ele;
-        points.forEach((point) => {
-          const squareIndex = this.grid[point[0]][point[1]];
-          squareIndex.on = new GamePiece(
-            name,
-            color,
-            point,
-            this.movementArray
-          );
-        });
-      })
-    );
-  }
-
-  createGrid(): Square[][] {
-    return Array.from({ length: this.boardSize }, (array, idx) =>
-      Array.from({ length: this.boardSize }, (obj, idx2) => ({
+  private createBoard() {
+    return Array.from({ length: this.boardSize }, (_, idx) =>
+      Array.from({ length: this.boardSize }, (_, idx2) => ({
         square: this.columnNames[idx][0] + (idx2 + 1),
         on: undefined,
       }))
     );
   }
 
-  resetBoard() {
-    this.grid = this.createGrid();
-    this.setPieces();
+  private setPieces() {
+    const startingPoints = this.getStartingPositions();
+    for (let i = 0; i < startingPoints.length; i++) {
+      for (let j = 0; j < startingPoints[i].length; j++) {
+        const { name, color, points } = startingPoints[i][j];
+        points?.forEach((point) => {
+          const square = this.board[point[1]][point[0]];
+          square.on = new GamePiece(name, color, point);
+        });
+      }
+    }
+  }
+
+  private getStartingPositions(): {
+    name: string;
+    color: string;
+    points: Point[];
+  }[][] {
+    const pieceNames = ["Pawn", "Rook", "Bishop", "Knight", "King", "Queen"];
+    const colors = ["White", "Black"];
+    return colors.map((color) => {
+      return pieceNames.map((name) => {
+        let points: Point[] = [];
+        switch (name) {
+          case "Pawn":
+            points = Array.from({ length: 8 }, (_, i) => [
+              i,
+              color === "White" ? 1 : 6,
+            ]);
+            break;
+          case "Rook":
+            points = [
+              [0, color === "White" ? 0 : 7],
+              [7, color === "White" ? 0 : 7],
+            ];
+            break;
+          case "Bishop":
+            points = [
+              [2, color === "White" ? 0 : 7],
+              [5, color === "White" ? 0 : 7],
+            ];
+            break;
+          case "Knight":
+            points = [
+              [1, color === "White" ? 0 : 7],
+              [6, color === "White" ? 0 : 7],
+            ];
+            break;
+          case "King":
+            points = [[4, color === "White" ? 0 : 7]];
+            break;
+          case "Queen":
+            points = [[3, color === "White" ? 0 : 7]];
+            break;
+        }
+        return { name, color, points };
+      });
+    });
   }
 }
 
