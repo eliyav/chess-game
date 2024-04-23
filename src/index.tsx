14bb982fb2cb2990dd013935e9e64ed6a8b295fb
Eliya Vahl <eliyavahl@gmail.com>
2024-04-23 21:29:53 +0300

    refactor: remove auth0

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import App from "./app";
-import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
@@ -11,15 +10,7 @@ const root = ReactDOMClient.createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain="chess-game.eu.auth0.com"
-        clientId="T3hIqwKbQqtTxqUJUcqJILVJ5fTEOAJK"
-        redirectUri={window.location.origin}
-        audience="chess-game API"
-        scope="openid profile email"
-      >
-        <App />
-      </Auth0Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
