7c438db88eb7d83413dead310b5884e92c40b50a
Eliya Vahl <eliyavahl@gmail.com>
2024-06-21 11:23:21 +0300

    fix: server path serving to index.html as SPA uses routing once loaded

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { fileURLToPath } from "node:url";
+import path from "path";
 
 const clientPath = fileURLToPath(new URL("../client", import.meta.url));
 
 const app = express();
 app.use(express.static(clientPath));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(clientPath, "index.html"));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
