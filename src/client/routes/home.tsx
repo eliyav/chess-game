11bb484c1dd651df1e38f267e696bfc640cdb67f
Eliya Vahl <eliyavahl@gmail.com>
2024-08-21 00:35:00 +0300

    fix: github icon to bottom right

--- src/client/routes/home.tsx
+++ src/client/routes/home.tsx
@@ -8,9 +8,7 @@ export const Home: React.FC = () => {
       <div className="header glass-dark">
         <h1 className="title">3D CHESS</h1>
         <p className="description">Play for free!</p>
-      </div>
-      <div className="icons">
-        <SvgGithub className="svg-icon" />
+        <SvgGithub className="svg-icon bottom-right" />
       </div>
       <div className="footer">
         <Link className={"btn glass-light"} to="/lobby">
