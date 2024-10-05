b57c885b78b5537fb8b99921a41e94fe78874562
Eliya Vahl <eliyavahl@gmail.com>
2024-10-05 23:07:43 +0300

    feat: updated PWA caching and crawl robots.txt

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,49 @@
-const CACHE_NAME = `3d-chess-v1`;
+const expectedCaches = [`3d-chess-v1`];
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      cache.addAll(["/", "/index.js", "/*.gltf"]);
+      const cache = await caches.open(expectedCaches.at(-1));
+      cache.addAll([
+        "/board-WBQHAPHW.gltf",
+        "/pawnv3-WMTUALHL.gltf",
+        "/bishopv3-4YRUFXLK.gltf",
+        "/knightv3-UB2HHP4W.gltf",
+        "/rookv3-ELRYDWNO.gltf",
+        "/queenv3-J76JIKCJ.gltf",
+        "/kingv3-KRLCVILK.gltf",
+        "/pawn-animation-GH4YVD7P.gltf",
+        "/bishop-animation-5YBADP5H.gltf",
+        "/knight-animation-FIC5EOG5.gltf",
+        "/rook-animation-UXA6GLY5.gltf",
+        "/queen-animation-A56RHX6J.gltf",
+        "/click-2VCHPPVR.mp3",
+        "/crumble-BBBHSYQE.mp3",
+        "/tube-ZB5725HQ.png",
+      ]);
+    })()
+  );
+});
+
+//Activation of service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cacheKeys = await caches.keys();
+      try {
+        await Promise.all(
+          cacheKeys.map((cacheKey) => {
+            if (!expectedCaches.includes(cacheKey)) {
+              return caches.delete(cacheKey);
+            }
+          })
+        );
+      } catch (e) {
+        console.error(e);
+      } finally {
+        console.log("SW now ready to handle fetches!");
+      }
     })()
   );
 });
@@ -13,22 +51,41 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
-      const cache = await caches.open(CACHE_NAME);
-
+      if (event.request.method !== "GET") {
+        return;
+      }
+      const cache = await caches.open(expectedCaches.at(-1));
       // Get the resource from the cache.
       const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        return cachedResponse;
+      //If its not a 3d model or image or audio, then check if the resource is modified since cached
+      if (
+        !event.request.url.includes(".gltf") ||
+        !event.request.url.includes(".png") ||
+        !event.request.url.includes(".mp3")
+      ) {
+        const request = event.request.clone();
+        const modifiedRequest = new Request(request);
+        if (cachedResponse) {
+          const lastModified = cachedResponse.headers.get("Last-Modified");
+          modifiedRequest.headers.set("If-Modified-Since", lastModified);
+        }
+        const response = await fetch(modifiedRequest);
+        //If response 304, then the resource is not modified since cached
+        if (response.status === 304) {
+          return cachedResponse;
+        }
+        // Cache the new response
+        cache.put(event.request, response.clone());
+        return response;
       } else {
-        try {
+        if (cachedResponse) {
+          return cachedResponse;
+        } else {
           // If the resource was not in the cache, try the network.
           const fetchResponse = await fetch(event.request);
-
           // Save the resource in the cache and return it.
           cache.put(event.request, fetchResponse.clone());
           return fetchResponse;
-        } catch (e) {
-          // The network failed.
         }
       }
     })()
