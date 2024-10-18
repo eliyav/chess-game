22dda71ddb3e7847a75423e1e6579bf18b54b9a8
Eliya Vahl <eliyavahl@gmail.com>
2024-10-18 20:11:33 +0300

    build: esbuild cli api into a config file for readability/maintainability (#105)

---
+++ esbuild.config.js
@@ -0,0 +1,64 @@
+import { build, context } from "esbuild";
+import fs from "node:fs/promises";
+
+const isWatch = process.argv.includes("--watch") || process.argv.includes("-w");
+const outPath = new URL("dist/", import.meta.url);
+const clientPath = new URL("dist/client", import.meta.url);
+const publicPath = new URL("public/", import.meta.url);
+
+/** @type {import('esbuild').BuildOptions} */
+const commonBuildOptions = {
+  logLevel: "info",
+  color: true,
+  outdir: "dist",
+  legalComments: "none",
+  bundle: true,
+  target: "es2022",
+  loader: {
+    ".png": "file",
+    ".jpeg": "file",
+    ".jpg": "file",
+    ".svg": "file",
+    ".mp3": "file",
+    ".gltf": "file",
+    ".webp": "file",
+  },
+  minify: !isWatch,
+  sourcemap: isWatch,
+  define: {
+    "process.env.NODE_ENV": isWatch ? '"development"' : '"production"',
+  },
+};
+
+/** @type {import('esbuild').BuildOptions} */
+const nodeEsmBundles = {
+  platform: "node",
+  ...commonBuildOptions,
+  format: "esm",
+  entryPoints: ["src/server/index.ts"],
+  outdir: "dist/server",
+  packages: "external",
+};
+
+/** @type {import('esbuild').BuildOptions} */
+const browserEsmBundle = {
+  ...commonBuildOptions,
+  format: "esm",
+  entryPoints: ["src/client/index.tsx"],
+  outdir: "dist/client",
+};
+
+await fs.rm(outPath, { recursive: true, force: true });
+await fs.mkdir(outPath, { recursive: true });
+await fs.cp(publicPath, clientPath, { recursive: true });
+
+const bundles = [nodeEsmBundles, browserEsmBundle];
+
+if (isWatch) {
+  const buildContexts = await Promise.all(
+    bundles.map((bundle) => context(bundle))
+  );
+  await Promise.all(buildContexts.map((context) => context.watch()));
+} else {
+  await Promise.all(bundles.map((bundle) => build(bundle)));
+}
