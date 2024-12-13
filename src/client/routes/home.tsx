7fc7da8c5ec6367f06d46a251ce0cfaf3e8f9dc3
Eliya Vahl <eliyavahl@gmail.com>
2024-12-13 20:26:58 +0200

    style: add style breakpoint for home

--- src/client/routes/home.tsx
+++ src/client/routes/home.tsx
@@ -7,8 +7,8 @@ import { APP_ROUTES } from "../../shared/routes";
 export const Home: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <div className="grid grid-rows-5 h-full">
-      <div className="relative glass dark-pane user-select-none p-6 m-6 ">
+    <div className="grid grid-rows-5 h-full md:w-1/2 md:m-auto">
+      <div className="relative glass dark-pane user-select-none p-6 m-6">
         <h1 className="text-3xl text-white font-bold italic pb-2">3D CHESS</h1>
         <p className="italic pb-2 text-white">Play for free!</p>
         <SvgGithub
@@ -17,7 +17,7 @@ export const Home: React.FC = () => {
         />
       </div>
       <SelectionButton
-        customClass="m-10 row-start-5 font-bold text-2xl border-2 border-white italic tracking-widest hover:opacity-80"
+        customClass="m-10 row-start-5 font-bold text-2xl border-2 border-white italic tracking-widest hover:opacity-80 md:w-1/2 md:justify-self-center"
         text="Play"
         onClick={() => navigate(APP_ROUTES.Lobby)}
       />
