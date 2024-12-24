c883bc11ab5588906551bc033d16274d84b08169
Eliya Vahl <eliyavahl@gmail.com>
2024-12-24 21:52:46 +0200

    remove: console.logs

--- public/bot-worker.js
+++ public/bot-worker.js
@@ -2,9 +2,7 @@ importScripts("./game-logic.js");
 
 const game = new Game();
 
-console.log(game);
 self.onmessage = (e) => {
-  console.log("Worker received message", e.data);
   if (e.data.type === "move") {
     game.move(e.data.data);
   } else if (e.data.type === "start") {
