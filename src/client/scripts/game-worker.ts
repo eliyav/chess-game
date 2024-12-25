80a8b327c1aea3c0c931458500ee4b2003f9fb38
Eliya Vahl <eliyavahl@gmail.com>
2024-12-25 03:12:29 +0200

    feat: local match web worker

--- src/client/scripts/game-worker.ts
+++ src/client/scripts/game-worker.ts
@@ -1,23 +1,60 @@
+import { Point } from "../../shared/game";
 import Game from "../game-logic/game";
 const game = new Game();
 
-self.onmessage = (e) => {
+function requestMove({
+  maximizingPlayer,
+  depth,
+}: {
+  maximizingPlayer: boolean;
+  depth: number;
+}) {
+  const bestMove = game.getBestMove({
+    depth,
+    maximizingPlayer,
+  });
+  if (!bestMove) return;
+  game.move(bestMove);
+  self.postMessage({ type: "move", move: bestMove });
+}
+
+self.onmessage = (e: WorkerEvents) => {
   if (e.data.type === "move") {
     game.move(e.data.data);
-  } else if (e.data.type === "start") {
+    requestMove({
+      maximizingPlayer: e.data.data.maximizingPlayer,
+      depth: e.data.data.depth,
+    });
+  } else if (e.data.type === "request-move") {
+    requestMove(e.data.data);
+  } else if (e.data.type === "start" || e.data.type === "reset") {
     game.resetGame();
   } else if (e.data.type === "undo") {
     game.undoTurn();
-  } else if (e.data.type === "request-move") {
-    const bestMove = game.getBestMove({
-      depth: e.data.depth,
-      maximizingPlayer: e.data.maximizingPlayer,
-    });
-    if (!bestMove) {
-      // self.postMessage({ type: "game-over" });
-      return;
-    }
-    game.move(bestMove);
-    self.postMessage({ type: "move", move: bestMove });
+  } else {
+    console.error("Invalid message type");
   }
+};
+
+type WorkerEvents = {
+  data:
+    | {
+        type: "request-move";
+        data: {
+          depth: number;
+          maximizingPlayer: boolean;
+        };
+      }
+    | {
+        type: "move";
+        data: {
+          origin: Point;
+          target: Point;
+          depth: number;
+          maximizingPlayer: boolean;
+        };
+      }
+    | {
+        type: "start" | "reset" | "undo";
+      };
 };
