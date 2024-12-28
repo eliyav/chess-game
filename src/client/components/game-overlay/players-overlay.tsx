ff9350e36620608fdcad18bd37eaaf14cb314452
Eliya Vahl <eliyavahl@gmail.com>
2024-12-28 15:21:35 +0200

    feat: player overview in game

--- src/client/components/game-overlay/players-overlay.tsx
+++ src/client/components/game-overlay/players-overlay.tsx
@@ -1,50 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import { Player, TEAM } from "../../../shared/match";
 import { Controller } from "../../match-logic/controller";
+import { ClockIcon } from "../svg/clock-icon";
 
 export const PlayersOverlay: React.FC<{
   players: Player[];
   info: ReturnType<Controller["info"]> | null;
 }> = ({ players, info }) => {
   return (
-    <div className="flex justify-center items-center h-full  p-2 text-white rounded-t-lg max-w-[600px] m-auto gap-x-4">
+    <div className="h-full p-2 text-white rounded-t-lg max-w-[600px] m-auto gap-x-4">
       {players.map((player, index) => (
-        <div
-          key={index}
-          className="flex flex-col items-center bg-slate-700 rounded-lg p-4 relative w-1/2 md:w-1/3"
-        >
-          <p className="font-bold text-lg">{player.name}</p>
-          <div className="flex items-center mt-2">
-            <svg
-              className="w-4 h-4 mr-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+        <div className="inline-block w-1/2" key={index}>
+          <div className="flex flex-col items-center bg-slate-700 rounded-lg p-2 m-2 relative md:w-1/3 transition-all duration-500 ease-in-out transform hover:scale-105">
+            <div
+              className={`absolute top-2 ${
+                player.team === TEAM.WHITE
+                  ? "left-2 bg-white"
+                  : "right-2 bg-black"
+              } rounded-full w-4 h-4 ${
+                info?.currentTeam === player.team ? "animate-pulse" : ""
+              }`}
+            ></div>
+            <p className="font-bold text-lg whitespace-nowrap overflow-hidden text-ellipsis w-3/4 max-w-3/4">
+              {player.name}
+            </p>
+            <div
+              className={`flex items-center justify-center mt-2 transition-all duration-1000 ease-in-out ${
+                info?.time ? "h-10" : "h-0 opacity-0"
+              }`}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              <ClockIcon
+                className={`w-8 h-8 mr-1 ${
+                  info?.currentTeam === player.team
+                    ? "animate-pulse text-red-500"
+                    : ""
+                }`}
               />
-            </svg>
-            <p className="text-sm">05:10</p>
-          </div>
-          <div className="absolute top-2 right-2">
-            {player.team === TEAM.WHITE ? (
-              <div
-                className={`w-4 h-4 bg-white rounded-full ${
-                  info?.currentTeam === TEAM.WHITE ? "animate-pulse" : ""
-                }`}
-              ></div>
-            ) : (
-              <div
-                className={`w-4 h-4 bg-black rounded-full ${
-                  info?.currentTeam === TEAM.BLACK ? "animate-pulse" : ""
-                }`}
-              ></div>
-            )}
+              <div className="flex items-center space-x-1 text-lg">
+                <span className="font-mono">{info?.time}</span>
+              </div>
+            </div>
           </div>
         </div>
       ))}
